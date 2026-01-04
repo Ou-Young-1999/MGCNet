365a952ff13173cfb1d641b0aee2f91cbed08a12
Ou-Young-1999 <50587400+Ou-Young-1999@users.noreply.github.com>
2026-01-04 09:47:01 +0800

    Update run_experiments.py

--- run_experiments.py
+++ run_experiments.py
@@ -2,14 +2,11 @@
 import os
 
 commands = [
+    "python train.py --config ./config/bgm/bgm_afdb_1.yaml",
     "python test.py --config ./config/bgm/bgm_afdb_1.yaml",
-    "python test.py --config ./config/bgm/bgm_afdb_2.yaml",
-    "python test.py --config ./config/bgm/bgm_afdb_3.yaml",
-    "python test.py --config ./config/bgm/bgm_afdb_4.yaml",
-    "python test.py --config ./config/bgm/bgm_afdb_5.yaml",
-
 ]
 
 for cmd in commands:
     print(f"Running: {cmd}")
+
     os.system(cmd)
